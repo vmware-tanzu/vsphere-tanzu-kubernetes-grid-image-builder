2d712e3a816c0985a4b5832f9a294d23a1280fa6
Sriraman S <sriraman.dev@gmail.com>
2025-01-27 19:04:00 +0530

    Update playbook to actuall call the pin command
    Signed-off-by: Sriraman S <sriraman.dev@gmail.com>

--- ansible/files/scripts/docker_registry_ops.py
+++ ansible/files/scripts/docker_registry_ops.py
@@ -6,36 +6,32 @@ IMAGE_NAME = "docker.io/vmware/docker-registry"
 PAUSE_IMAGE_NAME = "localhost:5000/vmware.io/pause"
 LABEL = "io.cri-containerd.pinned=pinned"
 
-def get_registry_version():
+def get_image_version(image_name):
     cp = subprocess.run(["crictl", "images", "-o", "json"], capture_output=True, text=True)
     cp.check_returncode()
     images = json.loads(cp.stdout)["images"]
     for image in images:
         for repo_tag in image["repoTags"]:
-            if repo_tag.startswith(IMAGE_NAME):
+            if repo_tag.startswith(image_name):
                 return repo_tag.split(":")[-1]
     else:
-        raise Exception(f"No image with name {IMAGE_NAME} found")
+        raise Exception(f"No image with name {image_name} found")
+
+def get_registry_version():
+    return get_image_version(IMAGE_NAME)
 
 def get_pause_version():
-    cp = subprocess.run(["crictl", "images", "-o", "json"], capture_output=True, text=True)
-    cp.check_returncode()
-    images = json.loads(cp.stdout)["images"]
-    for image in images:
-        for repo_tag in image["repoTags"]:
-            if repo_tag.startswith(PAUSE_IMAGE_NAME):
-                return repo_tag.split(":")[-1]
-    else:
-        raise Exception(f"No image with name {PAUSE_IMAGE_NAME} found")
+    return get_image_version(PAUSE_IMAGE_NAME)
+
+def apply_label(image):
+    subprocess.run(["ctr", "-n", "k8s.io", "images", "label", image, LABEL], check=True)
 
 def pin_image():
     image_name_with_version = IMAGE_NAME + ":" + get_registry_version()
-    image_name_with_latest = IMAGE_NAME + ":" + "latest"
-    subprocess.run(["ctr", "-n", "k8s.io", "images", "label", image_name_with_version, LABEL], check=True)
-    subprocess.run(["ctr", "-n", "k8s.io", "images", "label", image_name_with_latest, LABEL], check=False)
+    apply_label(image_name_with_version)
 
     pause_image_name_with_version = PAUSE_IMAGE_NAME + ":" + get_pause_version()
-    subprocess.run(["ctr", "-n", "k8s.io", "images", "label", pause_image_name_with_version, LABEL], check=True)
+    apply_label(pause_image_name_with_version)
 
 def main():
     parser = argparse.ArgumentParser(
